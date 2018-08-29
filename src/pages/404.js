63d226e52fdcc0e1c4aee7fdff92ded8efb30b11
Ctrivera19 <ctrivera19@gmail.com>
2018-08-28 19:13:22 -0700

    purge

--- src/pages/404.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Box from 'components/box';
-import Layout from 'components/layout';
-
-export default () => (
-  <Layout>
-    <Box>Not found.</Box>
-  </Layout>
-);
